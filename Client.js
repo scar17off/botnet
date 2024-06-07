b4f9016d019d9e0edc160c76ee855e59531f6cd7
scar17off <scar17off@gmail.com>
2024-06-08 01:40:17 +0300

    Enhanced code appearance

--- Client.js
+++ Client.js
@@ -2,14 +2,14 @@ class Client {
 	constructor(socket) {
 		this.ws = socket;
 		this.sid = socket.id;
-		this.id = global.lastid;
+		this.id = global.lastId++;
+		this.connected = false;
 		this.controlled = false;
 		this.controller = 0;
 		this.markedController = false;
-		this.ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
+		this.ip = (socket.handshake.headers['x-forwarded-for'] || socket.handshake.address).split(',')[0];
 		if(this.ip == "::1") this.ip = "localhost";
-		global.lastid++;
-	};
-};
+	}
+}
 
 module.exports = Client;
